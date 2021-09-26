dc0f1cc41d6c009ce556b425177d571c5f15e34f
Alexis DRAI <drai.alexis@gmail.com>
2021-09-26 13:41:07 +0200

    Debug iterating through matrices and change scanf to strtof
    A(1,3) was being overridden by A (2,1). Added a pointer
    incrementation at the end of each row.
    scanf was causing problems with type validation in a
    while loop, strtof is more clunky but more usable

--- TP_1/ex9.h
+++ TP_1/ex9.h
@@ -5,24 +5,34 @@
 #ifndef Y2_C_EX9_H
 #define Y2_C_EX9_H
 
+#define GET_INPUT  printf("row: %d | col: %d | >", i + 1, j + 1); fgets(input, 15, stdin); x = strtof(input, &endptr); printf("%f\n", x);
+
 void fillMat2d(float *mat, int rows, int cols) {
-    printf("Please select floats as coefficients for this matrix (i = %d, j = %d)\n", rows, cols);
+    printf("Please select floats as coefficients for this matrix (rows = %d, columns = %d)\n", rows, cols);
     float x;
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            printf(">");
-            scanf("%f", &x);
+            char input[16]; char *endptr;//arbitrarily decide how many chars can be used to make any x, for now
+
+            GET_INPUT
+
+            while (x == 0 && endptr == input) {
+                GET_INPUT
+            }
+
             *(mat + (i * rows) + j) = x;
         }
+        mat++;
     }
 }
 
-void showMat2d(const float *matrix, int rows, int cols) {
+void showMat2d(const float *mat, int rows, int cols) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            printf("%+02.2f ", *(matrix + (i * rows) + j));
+            printf("%+02.2f ", *(mat + (i * rows) + j));
         }
         printf("\n");
+        mat++;
     }
 }
 
