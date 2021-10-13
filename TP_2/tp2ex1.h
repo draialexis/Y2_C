ab7f0fddff966d010773f488b885d8848150abc9
Alexis DRAI <drai.alexis@gmail.com>
2021-10-13 15:18:48 +0200

    Debug ex2, perfect copies would cause failures
    because the check for EOF happened later than the check for non-chars

--- TP_2/tp2ex1.h
+++ TP_2/tp2ex1.h
@@ -5,7 +5,8 @@
 #ifndef Y2_C_TP2EX1_H
 #define Y2_C_TP2EX1_H
 
-void copyFile(char *f1_name, char *f2_name) {
+void copyFile(char *f1_name, char *f2_name)
+{
     int c;
 
     FILE *f1 = fopen(f1_name, "r");
@@ -14,12 +15,15 @@ void copyFile(char *f1_name, char *f2_name) {
     FILE *f2 = fopen(f2_name, "w");
     checkFopen(f2);
 
-    while (1) {
+    while (1)
+    {
         c = fgetc(f1);
-        if (feof(f1)) {
+        if (feof(f1))
+        {
             break;
         }
-        if (c < 0 || c > 255 || fputc(c, f2) == EOF) {
+        if (c < 0 || c > 255 || fputc(c, f2) == EOF)
+        {
             //writes each character while checking if the writing was successful
             printf("non-char detected or fputc failure\n");
             FAIL_OUT
@@ -29,7 +33,8 @@ void copyFile(char *f1_name, char *f2_name) {
     fclose(f2);
 }
 
-void showDiff(char *f1_name, char *f2_name) {
+void showDiff(char *f1_name, char *f2_name)
+{
     int c_a, c_b;
     int line_num = 1;
 
@@ -39,22 +44,29 @@ void showDiff(char *f1_name, char *f2_name) {
     FILE *f2 = fopen(f2_name, "r");
     checkFopen(f2);
 
-    while (1) {
+    while (1)
+    {
         c_a = fgetc(f1);
         c_b = fgetc(f2);
-        if (c_a != c_b) {
+        if (c_a != c_b)
+        {
             printf("difference detected at line %d:\n"
-                   "%c != %c\n", line_num, c_a, c_b);
+                   "%c != %c\n",
+                   line_num, c_a, c_b);
             break;
         }
-        if (c_a < 0 || c_a > 255 || c_b < 0 || c_b > 255) {
-            printf("non-char detected\n");
+        if (feof(f1) && feof(f2))
+        {
+            printf("perfect copy\n");
+            break;
+        }
+        if (c_a < 0 || c_a > 255 || c_b < 0 || c_b > 255)
+        {
+            printf("non-char detected at line %d\n", line_num);
             FAIL_OUT
         }
-        if (feof(f1)) {
-            break;
-        }
-        if (c_a == '\n') {
+        if (c_a == '\n')
+        {
             line_num++;
         }
     }
