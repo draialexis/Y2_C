259765e38408497b1733e11d932dfc543e1f61a8
Alexis DRAI <drai.alexis@gmail.com>
2021-10-27 16:05:22 +0200

    Start ex2, run into weird IDE bug, go to work

--- TP_3/main.c
+++ TP_3/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef char *str;
 
 #include "toolbox.h"
 #include "tp3ex1.h"
+#include "tp3ex2.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +17,11 @@ int main(int argc, char *argv[])
         str f_name = argv[1];
         str clip = argv[2];
         wri2k(f_name, clip);
+    }
+    else
+    {
+        str nums = "456 3 -59 11";
+        int *arr = decoupage(nums);
     }
     return 0;
 }
