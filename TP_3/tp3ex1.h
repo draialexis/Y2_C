894ce4ab53d4074495b50b38dc4ec7069c0dd086
Alexis DRAI <drai.alexis@gmail.com>
2021-10-27 15:43:40 +0200

    Investigate, test program

--- TP_3/tp3ex1.h
+++ TP_3/tp3ex1.h
@@ -13,8 +13,15 @@ void wri2k(str f_name, str clip)
     for (int i = 0; i < 2000; i++)
     {
         fputs(clip, fh);
+        usleep((rand() % 100)+1); //make the program sleep for 1 to 100 milsec
     }
     fclose(fh);
 }
+
+// when launching the same program twice at the same time, both strings 
+// get written down
+// it seems 2000 * the size of the largest of the two strings is allocated to be
+// written in the file, the smallest is written 2000 times, and whatever space there was
+// left was used to print a few instances of the largest string
 
 #endif //Y2_C_TP3EX1_H
