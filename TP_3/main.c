0a8d5bf633d7fa1604dc9d70743b696216377674
Alexis DRAI <drai.alexis@gmail.com>
2021-10-28 13:35:44 +0200

    Place pre-proc vars in the main again

--- TP_3/main.c
+++ TP_3/main.c
@@ -4,22 +4,23 @@
 #include <ctype.h>
 #include <string.h>
 
+#define DEBUG printf("file %s; line %d\n", __FILE__, __LINE__);
+#define FAIL_OUT exit(EXIT_FAILURE);
+#define MALLOC_FAIL printf("!_malloc failed_!\n"); DEBUG FAIL_OUT
+#define EMPTY_OR_NULL printf("this data structure doesn't have a positive integer value as its size, or the pointer to it isn't valid\n"); DEBUG FAIL_OUT;
+
 typedef char *str;
 
 #include "toolbox.h"
 #include "tp3ex1.h"
 #include "tp3ex2.h"
 
-int main(int argc, char *argv[])
-{
-    if (argc == 3)
-    {
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
         str f_name = argv[1];
         str clip = argv[2];
         wri2k(f_name, clip);
-    }
-    else
-    {
+    } else {
         str nums = "456 3 -59 11";
         int *arr = decoupage(nums);
     }
