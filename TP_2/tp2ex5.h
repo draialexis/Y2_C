b49946783f42a8d27cf4269744fb85d73158e29a
Alexis DRAI <drai.alexis@gmail.com>
2021-11-09 15:51:34 +0100

    Add txt files

--- TP_2/tp2ex5.h
+++ TP_2/tp2ex5.h
@@ -7,78 +7,75 @@
 
 #define BFR 512
 
-char *permuteString(char *str, int *arr)
-{
+char *permuteString(char *str, int *arr) {
     // TODO more user input validation? seems rough in C, esp. given the _cahier des charges_
 
-    if (strlen(str) > 0 && str != NULL && arr != NULL)
-    {
+    if (strlen(str) > 0 && str != NULL && arr != NULL) {
         char *res = malloc(sizeof(char) * strlen(str));
-        if (res == NULL)
-        {
+        if (res == NULL) {
             MALLOC_FAIL
         }
 
         sprintf(res, "%s", str); //initializing the string
 
-        for (int i = 0; i < strlen(str); i++)
-        {
+        for (int i = 0; i < strlen(str); i++) {
             res[arr[i]] = str[i];
             printf("res[%d] = %c / str[%d] = %c\n", arr[i], res[arr[i]], i, str[i]);
         }
         return res;
-    }
-    else
-    {
+    } else {
         EMPTY_OR_NULL
         return NULL;
     }
 }
 
-void encode(char *txt_f_name, char *perm_f_name)
-{
+void encode(char *txt_f_name, char *perm_f_name) {
+    printf("%s and %s\n", txt_f_name, perm_f_name);
     FILE *fp_txt = fopen(txt_f_name, "r");
     checkFopen(fp_txt);
     FILE *fp_perm = fopen(perm_f_name, "r");
     checkFopen(fp_perm);
 
     int c;
-    char *txt = (char *)malloc(sizeof(char) * BFR);
+    char *txt = (char *) malloc(sizeof(char) * BFR);
     sprintf(txt, "%c", '\0');
-    int *perm = (int *)malloc(sizeof(int) * BFR);
+    int *perm = (int *) malloc(sizeof(int) * BFR);
 
-    if (txt == NULL || perm == NULL)
-    {
+    if (txt == NULL || perm == NULL) {
         MALLOC_FAIL
     }
 
-    while (1)
-    {
+    while (1) {
         c = fgetc(fp_txt);
-        if (feof(fp_txt))
-        {
+        if (feof(fp_txt)) {
             break;
         }
         sprintf(txt + strlen(txt), "%c", c);
     }
-
-    char *txt_perm = (char *)malloc(sizeof(char) * BFR);
+    printf("%s\n", txt);
+    DEBUG
+    char *txt_perm = (char *) malloc(sizeof(char) * BFR);
     sprintf(txt_perm, "%c", '\0');
 
     int n = 0;
-    while (1)
-    {
+    while (1) {
         c = fgetc(fp_perm);
-        if (feof(fp_perm))
-        {
+        if (feof(fp_perm)) {
             break;
         }
         // vomitting face emoji
-        sprintf(txt_perm, "%c", c);
+        sprintf(txt_perm + n, "%c", c);
         perm[n] = atoi(txt_perm);
         n++;
     }
-
+    DEBUG
+    printf("%s\n", txt_perm);
+    DEBUG
+    for (int i = 0; i < n; ++i) {
+        printf("%d", perm[i]);
+    }
+    printf("\n");
+    DEBUG
     // printing file contents, debugging
     /*
     printf("%s\n", txt);
@@ -108,15 +105,15 @@ void encode(char *txt_f_name, char *perm_f_name)
     fclose(fp_txt);
     fclose(fp_perm);
 
-    fp_txt = fopen(txt_f_name, "w");
-    fp_perm = fopen(perm_f_name, "w");
-
-    for (int i = 0; i < strlen(txt) / n; i++)
-    {
-
-    }
-
-    fclose(fp_txt);
-    fclose(fp_perm);
+//    fp_txt = fopen(txt_f_name, "w");
+//    fp_perm = fopen(perm_f_name, "w");
+//
+//    for (int i = 0; i < strlen(txt) / n; i++) {
+//
+//    }
+//
+//    fclose(fp_txt);
+//    fclose(fp_perm);
 }
+
 #endif //Y2_C_TP2EX5_H
