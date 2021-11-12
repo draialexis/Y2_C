5ab61c2fe60cbaf835d5d177f4c5facaffeba882
Alexis DRAI <drai.alexis@gmail.com>
2021-11-12 18:53:47 +0100

    Do ex5: macros

--- TP_3/main.c
+++ TP_3/main.c
@@ -29,6 +29,27 @@ int main(int argc, char *argv[]) {
         //// if there is a non-int in our params, the program compiles, and executes, but the behavior is erroneous
         //// if n (the size) is greater than the number of other params, behavior is erroneous
         //// if n is less, some params get ignored of course
+        //5)
+        printf("%f\n", MAX(45.5, 65.0));
+        printf("%d\n", MAX(45, 65));
+        printf("%d\n", MAX(151, MAX(45, 65)));
+        //// we can compare elements of various types in each call
+        //// but only elements of the same type within one call
+////        ({
+////            typeof(151) _a = (151);
+////            typeof(({
+////                typeof(45) _a = (45);
+////                typeof(65) _b = (65);
+////                _a > _b ? _a : _b;
+////            })) _b = (({
+////                typeof(45) _a = (45);
+////                typeof(65) _b = (65);
+////                _a > _b ? _a : _b;
+////            }));
+////            _a > _b ? _a : _b;
+////        });
+////    }
+
+        return 0;
     }
-    return 0;
 }
